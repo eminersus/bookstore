88fe6ca09a1a00211942b51b49f67c89e138ac12
emin <eminersus@gmail.com>
2024-06-15 03:35:53 +0300

    add basic pydantic models for Book, Author and Genre

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from .schemas import Book
 
 app = FastAPI()
 
+testData = {
+    "books": [
+        {
+            "title": "The Hobbit",
+            "publishedDate": "1937-09-21T00:00:00.000Z",
+            "authors": ["J.R.R. Tolkien"],
+            "genres": ["Fantasy"]
+        },
+        {
+            "title": "The Fellowship of the Ring",
+            "publishedDate": "1954-07-29T00:00:00.000Z",
+            "authors": ["J.R.R. Tolkien"],
+            "genres": ["Fantasy"]
+        }
+    ]
+}
+
 @app.get("/")
-def root_test():
+async def root_test():
     return {"message": "this is a root route test"}
 
+@app.get("/books")
+async def get_all_books() -> list[Book]:
+    return [Book(**book) for book in testData["books"]]
---
+++ app/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field, conlist
+from typing import List
+from datetime import datetime, date
+
+class Book(BaseModel):
+    title: str
+    published_date: date = Field(..., alias="publishedDate")
+    authors: List[str] = Field(..., min_length=1) # TODO: change these to @validate
+    genres: List[str] = Field(..., min_length=1) # TODO: change these to @validate
+
+class Author(BaseModel):
+    full_name: str = Field(..., alias="fullName")
+    birth_date: date = Field(..., alias="birthDate")
+    books: List[Book] = []
+
+class Genre(BaseModel):
+    name: str
+    genre_id: int = Field(..., alias="id")
+    parent_id: int = Field(..., alias="parentId")
+
