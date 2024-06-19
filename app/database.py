3a0edbcca8a36bc35f2f04fa3fb0ae8b50718713
emin <eminersus@gmail.com>
2024-06-19 23:41:25 +0300

    change project structure before routes implementation

--- app/database.py
+++ app/database.py
@@ -1,36 +1,40 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
 from .config import Settings
 from . import models
 from .seeder import get_seed_genre_data, insert_genre_data
-from contextlib import contextmanager
+from contextlib import contextmanager, asynccontextmanager
+from fastapi import FastAPI
 
 class Database:
     def __init__(self, url: str):
         self.url = url
         self.__engine = create_engine(self.url, connect_args={"check_same_thread": False})
         self.__SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.__engine)
-    def get_engine(self):
-        return self.__engine
-
-    def __get_session_local(self):
-        return self.__SessionLocal
     
-    @contextmanager
     def get_session(self):
-        db = self.__get_session_local()
+        db = self.__SessionLocal()
         try:
-            yield db()
+            yield db
         finally:
-            db().close()
+            db.close()
 
-    def init_db(self):
+    def init_db_tables(self):
         models.Base.metadata.create_all(bind=self.__engine)
 
     def insert_seed_genres_data(self):
-        with self.get_session() as session:
-            data = get_seed_genre_data()
-            insert_genre_data(data, session)
+        data = get_seed_genre_data()
+        db = self.__SessionLocal()
+        insert_genre_data(data, db)
+        db.close()
+
+    @asynccontextmanager
+    async def init_db(self, app: FastAPI):
+        self.init_db_tables()
+        self.insert_seed_genres_data()
+        yield
+        self.__engine.dispose()
 
 
 # Create a database instance
--- app/seeder.py
+++ app/seeder.py
@@ -1,17 +1,17 @@
 from json import load
-from .crud import create_genre
+from . import crud
 from . import schemas
 from sqlalchemy.orm import Session
 
 
 def get_seed_genre_data():
-    with open("./app/genre_tree.json") as file:
+    with open("./app/data/genre_tree.json") as file:
         genres = load(file)
         return genres
     
 def insert_genre_data(data, session: Session, parent_path="/"):
     for item in data:
         genre = schemas.GenreCreate(name=item["name"], path=f"{parent_path}{item["id"]}/")
-        create_genre(session, genre)
+        crud.create_genre(genre, session)
         if "children" in item:
             insert_genre_data(item["children"], session, genre.path)
